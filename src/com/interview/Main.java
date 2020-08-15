202b01aaa63e7df540633644eba45058d85721ad
sudheer ravuri <sudheerravuri@sudheers-MacBook-Pro.local>
2020-08-15 08:32:36 -0400

    init2

--- src/com/interview/Main.java
+++ src/com/interview/Main.java
@@ -1,23 +1,56 @@
 package com.interview;
 
+import java.math.BigInteger;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int[] memo = new int[5];
-        int result = fib(5,memo);
+
+        int n =1000;
+        long[] memo = new long[n+1];
+        //long result = fib(n, memo);// Will throw stack over flow
+
+        BigInteger result = fib3(n);
+
         System.out.println("Fib Result : "+result);
     }
 
-    static int fib(int n, int[] memo){
+    //Recursive Approach with Memoize
+    static long fib(int n, long[] memo){
         if(memo[n]!=0){
             return memo[n];
         }
         if(n==1 || n==2){
             return 1;
         }
-        int result = fib(n-1,memo)+ fib(n-2,memo);
+        long result = fib(n-1,memo)+ fib(n-2,memo);
+        memo[n]=result;
         return result;
     }
 
+
+//BottomUp Approach-- Most efficient
+    static long fib(int n){
+        long[] memo = new long[n+1];
+        memo[1]=1;
+        memo[2]=1;
+        for(int i=3;i<=n;i++){
+            memo[i]=memo[i-1]+memo[i-2];
+        }
+        return memo[n];
+    }
+//Using BigInteger
+    static BigInteger fib3(int n){
+        BigInteger a = BigInteger.valueOf(0);
+        BigInteger b = BigInteger.valueOf(1);
+        BigInteger c = BigInteger.valueOf(1);
+        for (int j=2 ; j<=n ; j++)
+        {
+            c =  a.add(b);
+            a = b;
+            b = c;
+        }
+        return a;
+    }
 }
